c218baf596c7e0ec90ffa30a05633bee71d165ec
Magiri1115 <busmagiri6@gmail.com>
2025-12-20 12:02:31 +0900

    Contactのメールフォームはいらない

--- components/sections/contact-section.tsx
+++ components/sections/contact-section.tsx
@@ -1,57 +1,30 @@
 export default function ContactSection() {
   return (
     <section id="contact-section">
+      <div className="contact-container">
+        <h2>CONTACT</h2>
+        <p>お気軽にお問い合わせください</p>
+      </div>
 
       {/* mail-content */}
-      <div className="mail-content">
+      <div className="contact-content">
+        <div className="mail-container">
 
-        {/* --- 名前と住所 --- */}
-        <div>
-          <div>
-            {/* name */}
-            <label htmlFor="name">name</label>
-            {/* text-name */}
-            <input id="name" type="text" className="text-name" />
-          </div>
 
-          <div>
-            {/* address */}
-            <label htmlFor="address">address</label>
-            {/* text-address */}
-            <input id="address" type="email" className="text-address" />
+          {/* --- SNSアイコンエリア --- */}
+          <div className="contact-icon-item">
+            <div className="contact-icon">
+              <img src="/icons/discord.svg" alt="Discord" />
+            </div>
+            <div className="contact-icon">
+              <img src="/icons/twitter.svg" alt="Twitter" />
+            </div>
+            <div className="contact-icon">
+              <img src="/icons/github.svg" alt="GitHub" />
+            </div>
           </div>
+          
         </div>
-
-        {/* --- 件名 (subject) --- */}
-        <div>
-          <label htmlFor="subject">subject</label>
-          <input id="subject" type="text" className="text-subject" />
-        </div>
-
-        {/* --- メッセージ (message) --- */}
-        <div>
-          <label htmlFor="message">messeage</label>
-          <textarea id="message" className="text-messeage"></textarea>
-        </div>
-
-        {/* --- 送信ボタン --- */}
-        <button type="submit" className="submit">
-          submit
-        </button>
-
-        {/* --- SNSアイコンエリア --- */}
-        <div className="sns-icon-item">
-          <div>
-            <img src="/discord-icon.svg" alt="Discord" />
-          </div>
-          <div>
-            <img src="/X-icon.svg" alt="X" />
-          </div>
-          <div>
-            <img src="/github-icon.svg" alt="GitHub" />
-          </div>
-        </div>
-
       </div>
     </section>
   );
